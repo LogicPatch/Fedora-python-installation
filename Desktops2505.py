5341a405ed5d57c1ab1161c5bb3c69122e79ff09
Oliver Prosenz <oliver.prosenz@gmx.net>
2025-11-13 19:37:50 +0100

    Add File Desktops2505.py

---
+++ Desktops2505.py
@@ -0,0 +1,31 @@
+import unicodedata
+import os
+import time
+
+# Skripte ausführen mit    $ sudo python3 0?-????.py
+# Zuletzt aktualisiert: 06.03.2023
+
+# Farbige print-Ausgaben
+reset='\33[0m'          # Keine Farbe. (Farbe zurücksetzen)
+lila='\33[35m'          # Lila (Installationsfragen)
+flila='\33[1;35m'       # fettes Lila (Installationsfragen)
+green='\33[32m'         # Grün (Alles O.K.)
+fgreen='\33[1;32m'      # fettes Grün (Alles O.K.)
+yellow='\33[1;33m'      # Gelb (Hinweismeldungen)
+gray='\33[1;30m'        #
+blue='\33[34m'          # Blau (Alternativ: Informationsausgaben)
+fblue='\33[1;34m'       # fettes Blau (Informationsausgaben)
+cyan='\33[36m'          # Cyan ()
+fcyan='\33[1;36m'       # fettes Cyan ()
+rot='\33[31m'           # Rot (Alternative: Fehler)
+frot='\33[1;31m'        # fettes Rot (Fehler)
+
+
+#------------------------------------------------------------------------------------------
+
+
+
+'''
+Ab den Themes werden hier alle weiteren Installationen festgehalten, damit Redundance bei weiteren Desktopinstallationen vermieden wird.
+   Die Komplettinstallation und die Installation weiterer Anwendungen wird weiterhin im entsprechenden Desktop-Script vorgenommen.
+'''
